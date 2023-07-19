acb6376565be3fa38a7fa06f83793fa6bf1dee0f
Worm <mwormley008@gmail.com>
2023-07-19 14:39:39 -0500

    working on saving

--- AIA.py
+++ AIA.py
@@ -1,10 +1,10 @@
 # Script to put in the expense categories
 
-import pyautogui, openpyxl, datetime, calendar
+import pyautogui, openpyxl, datetime, calendar, os
 from openpyxl import Workbook, load_workbook
 from datetime import datetime, timedelta, date
 
-from tkinter import Tk
+from tkinter import Tk, simpledialog
 from tkinter.filedialog import askopenfilename
 
 
@@ -12,7 +12,8 @@ from tkinter.filedialog import askopenfilename
 root = Tk()
 root.withdraw()  # Hide the root window
 initial_dir=r"\\WBR\data\shared\G702 & G703 Forms"
-workbook_path = askopenfilename(initial_dir=initial_dir)
+workbook_path = askopenfilename(initialdir=initial_dir)
+print(workbook_path)
 "\\WBR\data\shared\G702 & G703 Forms\41 North Contractors Raising Canes New Lenox 15682 3.xlsx"
 # Open AIA form based on invoice number
 # maybe I could do this with tkinter idk
@@ -32,20 +33,20 @@ Note that the askopenfilename() function returns the selected file path as a str
 
 Make sure to import the necessary modules (Tk and askopenfilename) from tkinter and create a Tk root window before using the askopenfilename() function.
 """
+new_invoice_number = simpledialog.askinteger("Invoice Prompt", "Enter the new invoice number:")
 
 # Page 1
 workbook = load_workbook(filename=workbook_path)
 sheet1 = workbook["G702"]
+
+# Increment application number
 application = sheet1["J3"]
+old_application_number = application.value
 print(application.value)
 application.value = application.value + 1
-# Increment application number
-# cell = j3
-
-
+new_application_number = application.value
 
 # increment work period
-# cell j7
 
 today = date.today()
 print(today)
@@ -56,6 +57,9 @@ print(workperiod)
 
 # change invoice number to the new invoice number
 # J9
+invoice_number = sheet1["J9"]
+old_invoice_number = invoice_number.value
+invoice_number.value = new_invoice_number
 
 # add old current payment E39 to previous payments E38
 sheet1["E38"].value += sheet1["E39"].value
@@ -70,12 +74,21 @@ for x, y in zip(prev_apps, this_period):
     sheet2[x].value += sheet2[y].value
     sheet2[y].value = 0
 
-workbook.save(workbook_path)
 
 # Add old this period to previous applications
 #D13 E13
 # Each new row increases by 4 (D17 E17)
 
 # Save a new file with new name
+"""
+new_workbook_path = workbook_path.replace(str(old_invoice_number), str(new_invoice_number))
+new_workbook_path = workbook_path.replace(str(old_application_number), str(new_application_number))
+
+print(new_workbook_path)
+print(type(new_workbook_path))
+
+new_workbook_path = os.path.normpath(new_workbook_path)
+print(new_workbook_path)
+workbook.save(new_workbook_path)"""
 
 # Add formulae to all cells 
