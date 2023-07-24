4e12fc9dba8d51895ed59f843abbd517e8c24171
Worm <mwormley008@gmail.com>
2023-07-24 12:46:43 -0500

    scanning

--- invoice_scan.py
+++ invoice_scan.py
@@ -2,8 +2,10 @@
 # this was written by GPT so there's going to be some trouble shooting involved
 
 import cv2
+import pypdf
 import pytesseract
 import pdf2image
+import os, re
 from pdf2image import convert_from_path
 
 # You must install tesseract on your machine and update the path below
@@ -21,11 +23,37 @@ def ocr_core(image):
 
     return text
 
-# Convert PDF to images
-pages = convert_from_path(r'C:\Users\Michael\Downloads\140338.pdf', dpi=300)
+def extract_text_from_pdf(file_path):
+    pdf_file_obj = open(file_path, 'rb')
+    pdf_reader = pypdf.PdfReader(pdf_file_obj)
+    num_pages = len(pdf_reader.pages)
+    text = ""
+    for page in range(num_pages):
+        #page_obj = pdf_reader.getPage(page)
+        page_obj = pdf_reader.pages[page]
+        text += page_obj.extract_text()
+    pdf_file_obj.close()
+    return text
 
-# Perform OCR on each image
-for i, page in enumerate(pages):
-    page.save('out.jpg', 'JPEG')
-    img = cv2.imread('out.jpg')
-    print(f"Content on page {i+1}:\n{ocr_core(img)}")
+
+folder_path = r'C:\Users\Michael\Desktop\python-work\Invoices'
+
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(".pdf"): # add any file type you want to process
+        print(os.path.join(folder_path, filename))
+        # Replace 'path_to_your_pdf' with the path to the PDF file you want to process
+        """ text = extract_text_from_pdf(os.path.join(folder_path, filename))
+        print(text) """
+        pages = convert_from_path(os.path.join(folder_path, filename), dpi=300)
+
+        # Perform OCR on each image
+        for i, page in enumerate(pages):
+            page.save('out.jpg', 'JPEG')
+            img = cv2.imread('out.jpg')
+            print(f"Content on page {i+1}:\n{ocr_core(img)}")
+            content = ocr_core(img)
+            if content.startswith("GEN"):
+                print("GEMCO")
+    else:
+        continue
