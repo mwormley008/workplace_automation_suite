a889361bdb1e43b6a848435d36c84b3d8da80a15
Worm <mwormley008@gmail.com>
2023-07-24 15:27:58 -0500

    grabs all relevant info from Gemco invoices

--- invoice_scan.py
+++ invoice_scan.py
@@ -8,7 +8,7 @@ import pdf2image
 import os, re, datetime
 import openpyxl
 from pdf2image import convert_from_path
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from datetime import date
 
 # You must install tesseract on your machine and update the path below
@@ -38,60 +38,119 @@ def extract_text_from_pdf(file_path):
     pdf_file_obj.close()
     return text
 
-wb = Workbook()
+def is_file_in_folder(filename, folder_path):
+    file_path = os.path.join(folder_path, filename)
+    return os.path.isfile(file_path)
 
-ws = wb.active
+
 
 file_name = f'{date.today()}_invoices.xlsx'
 
 folder_path = r'C:\Users\Michael\Desktop\python-work\Invoices'
 
+if is_file_in_folder(file_name, folder_path):
+    wb = load_workbook(os.path.join(folder_path, file_name))
+    ws = wb.active
+else:
+    wb = Workbook()
 
+    ws = wb.active
+
+    ws['A1'] = 'Vendor'
+    ws['B1'] = 'Date'
+    ws['C1'] = 'Number'
+    ws['D1'] = 'Amount Due'
+
+invoice_counter = len(ws['C'])
+unique_invoices = []
+
+for cell in ws['C']:
+    unique_invoices.append(cell.value)
+
+## Loops through the files in a chosen folder, extracts the relevant text from them
+## and puts them in an excel file
 for filename in os.listdir(folder_path):
     if filename.endswith(".pdf"): # add any file type you want to process
         print(os.path.join(folder_path, filename))
         # Replace 'path_to_your_pdf' with the path to the PDF file you want to process
         """ text = extract_text_from_pdf(os.path.join(folder_path, filename))
         print(text) """
+        
         pages = convert_from_path(os.path.join(folder_path, filename), dpi=300)
 
         # Perform OCR on each image
+        invoice_no = None
+
         for i, page in enumerate(pages):
+            match_counter = 0
             page.save('out.jpg', 'JPEG')
             img = cv2.imread('out.jpg')
-            print(f"Content on page {i+1}:\n{ocr_core(img)}")
+            print(f"Content on {ocr_core(img)}")
             content = ocr_core(img)
-            # Finds the vendor
-            if content.startswith("GEN"):
-                print("Vendor: GEMCO")
-
-            # Finds the date of the invoice    
-            date_match = re.search(r'invoice date[:\s]*([01]?\d/[0123]?\d/\d{2})', content, re.IGNORECASE)
-            if date_match:
-                # If a match was found, 'group(1)' contains the first parenthesized subgroup - the date.
-                invoice_date = date_match.group(1)
-                print(f"Found 'invoice date': {invoice_date}")
-            else:
-                print("No 'invoice date' found")
             
-            ## Finds the invoice number
+            ## Finds the invoice number and checks for uniqueness
+            inv_pattern = r'INVOICE\n\n(\d+-\d+)'
+            inv_pattern2 = r'main office (\d+-?\d*)'
+            page_pattern = r"page\s(\d+)\sof\s([2-9])"
 
-            inv_match = re.search(r'main office (\d+-?\d*)', content, re.IGNORECASE)
+
+
+            inv_match = re.search(inv_pattern, content, re.IGNORECASE)
+
             if inv_match:
-                # If a match was found, 'group(1)' contains the first parenthesized subgroup - the office number.
-                invoice_no = inv_match.group(1)
-                print(f"Found 'main office number': {invoice_no}")
-            else:
-                print("No 'main office number' found")
+                match_counter = 1
+            inv_match2 = re.search(inv_pattern2, content, re.IGNORECASE)
+            if inv_match2:
+                match_counter = 2
+            page_match = re.search(page_pattern, content, re.IGNORECASE)
+            print(inv_match)
+            if match_counter:
+                if match_counter == 1:
+                    invoice_no = inv_match.group(1)
+                else:
+                    invoice_no = inv_match2.group(1)
 
-            ## Finds the invoice amount due
-            total_match = re.search(r'balance \$([\d,]+\.\d{2})', content, re.IGNORECASE)
-            if total_match:
-                # If a match was found, 'group(1)' contains the first parenthesized subgroup - the balance amount.
-                balance_amount = total_match.group(1)
-                print(f"Found 'balance amount': ${balance_amount}")
+                print(f"Found 'invoice number': {invoice_no}")
+                if invoice_no not in unique_invoices:
+                    invoice_counter += 1
+                    unique_invoices.append(invoice_no)
+                    ws[f'C{invoice_counter}'] = f'{invoice_no}'
+                    
+                    # Finds the vendor
+                    if content.startswith("GEN"):
+                        print("Vendor: GEMCO")
+                        ws[f'A{invoice_counter}'] = 'Gemco'
+
+                    # Finds the date of the invoice    
+                    date_match = re.search(r'invoice date[:\s]*([01]?\d/[0123]?\d/\d{2})', content, re.IGNORECASE)
+                    if date_match:
+                        # If a match was found, 'group(1)' contains the first parenthesized subgroup - the date.
+                        invoice_date = date_match.group(1)
+                        print(f"Found 'invoice date': {invoice_date}")
+                        ws[f'B{invoice_counter}'] = f'{invoice_date}'
+                    else:
+                        print("No 'invoice date' found")
+
+                    ## Finds the invoice amount due
+                    total_match = re.search(r'balance \$([\d,]+\.\d{2})', content, re.IGNORECASE)
+                    if total_match:
+                        # If a match was found, 'group(1)' contains the first parenthesized subgroup - the balance amount.
+                        balance_amount = total_match.group(1)
+                        print(f"Found 'balance amount': ${balance_amount}")
+                        ws[f'D{invoice_counter}'] = balance_amount
+                    else:
+                        print("No 'balance amount' found")
+            if page_match:
+                total_match = re.search(r'balance \$([\d,]+\.\d{2})', content, re.IGNORECASE)
+                if total_match:
+                    # If a match was found, 'group(1)' contains the first parenthesized subgroup - the balance amount.
+                    balance_amount = total_match.group(1)
+                    print(f"Found 'balance amount': ${balance_amount}")
+                    ws[f'D{invoice_counter}'] = balance_amount
             else:
-                print("No 'balance amount' found")
+                print("No 'unique invoice number' found")
+            
+            
 
     else:
         continue
