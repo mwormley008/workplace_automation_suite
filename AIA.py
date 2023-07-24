fed18e1f5f0bb2b967462e767c813d4e7bf19f67
Worm <mwormley008@gmail.com>
2023-07-24 17:01:23 -0500

    working on new billing cycles

--- AIA.py
+++ AIA.py
@@ -1,30 +1,34 @@
 # Script to put in the expense categories
 
 import pyautogui, openpyxl, datetime, calendar, os, sys, re
-import subprocess
+import subprocess, time
 from openpyxl import Workbook, load_workbook
 from datetime import datetime, timedelta, date
 
 from tkinter import Tk, simpledialog, messagebox
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, Frame, Button
+from time import sleep
 
+class CustomDialog(simpledialog.Dialog):
+    def __init__(self, parent, title=None, button_names=None):
+        self.button_names = button_names
+        super().__init__(parent, title=title)
 
-# Create the Tkinter root window
-root = Tk()
-root.withdraw()  # Hide the root window
+    def body(self, master):
+        return None  # override body function to prevent an extra Entry widget
 
-response = messagebox.askyesno("Confirmation", "Do you want to start in Quickbooks?")
+    def buttonbox(self):
+        # add custom buttons
+        box = Frame(self)
+        for button in self.button_names:
+            Button(box, text=button, height=5, width=15, command=lambda text=button: self.ok(text)).pack(side="left", padx=5, pady=5)
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+        box.pack()
 
-if response:
-    qtoken = 1
-    exec(open('progress_invoice.py').read())
-else:
-    qtoken = 0
-
-
-old_invoice_number = simpledialog.askinteger("Invoice Prompt", "Enter the old invoice number:")
-
-initial_dir=r"\\WBR\data\shared\G702 & G703 Forms"
+    def ok(self, button_name, event=None):
+        self.result = button_name
+        self.destroy()
 
 def find_file_by_number(folder_path, target_number):
     pattern = re.compile(r'.*{}.*\.xlsx$'.format(target_number))
@@ -37,23 +41,57 @@ def find_file_by_number(folder_path, target_number):
     # If the file is not found
     return None
 
-# Example usage
-folder_path = initial_dir # Replace with the actual folder path
-target_number = old_invoice_number  # Replace with the specific number you are looking for
+# Create the Tkinter root window
+root = Tk()
+root.withdraw()  # Hide the root window
+noe_token = 0
+new_or_existing = CustomDialog(root, title="New or Existing", button_names=["New Billing Cycle", "Existing Billing Cycle"])
+print("You clicked:", new_or_existing.result)
 
-file_path = find_file_by_number(folder_path, target_number)
+if new_or_existing.result == 'New Billing Cycle':
+    noe_token = 1
 
-if file_path:
-    print(f"File found: {file_path}")
-    use_file = messagebox.askyesno("Confirmation", f"Do you want to use this file?/\n {file_path}")
-    if use_file:
-        workbook_path = file_path
-    else:
-        workbook_path = askopenfilename(initialdir=initial_dir)
+qb_response = messagebox.askyesno("Confirmation", "Do you want to start in Quickbooks?")
+sleep(1)
+
+# Going to need new_contract_invoice.py
+if qb_response and noe_token == 0:
+    qtoken = 1
+    exec(open('progress_invoice.py').read())
+elif qb_response and noe_token == 1:
+    qtoken = 1
+    contract_date = simpledialog.askinteger("Contract Prompt", "What is the contract date DD/MM/YY:")
+    exec(open('new_contract_invoice.py').read())
+elif qb_response == 0 and noe_token == 1:
+    qtoken = 0
+    contract_date = simpledialog.askinteger("Contract Prompt", "What is the contract date DD/MM/YY:")
 else:
-    print("File not found.")
-    workbook_path = askopenfilename(initialdir=initial_dir)
-print(workbook_path)
+    qtoken = 0
+
+
+initial_dir=r"\\WBR\data\shared\G702 & G703 Forms"
+
+folder_path = initial_dir 
+
+
+if noe_token == 0:
+    old_invoice_number = simpledialog.askinteger("Invoice Prompt", "Enter the old invoice number:")
+    target_number = old_invoice_number  # Replace with the specific number you are looking for
+
+    file_path = find_file_by_number(folder_path, target_number)
+    if file_path:
+        print(f"File found: {file_path}")
+        use_file = messagebox.askyesno("Confirmation", f"Do you want to use this file?/\n {file_path}")
+        if use_file:
+            workbook_path = file_path
+        else:
+            workbook_path = askopenfilename(initialdir=initial_dir)
+    else:
+        print("File not found.")
+        workbook_path = askopenfilename(initialdir=initial_dir)
+    print(workbook_path)
+else:
+    workbook_path = r"\\WBR\data\shared\G702 & G703 Forms\Excel Waiver 1 page.xlsx"
 
 
 if qtoken == 0:
@@ -64,12 +102,18 @@ if qtoken == 0:
 workbook = load_workbook(filename=workbook_path)
 sheet1 = workbook["G702"]
 
-# Increment application number
 application = sheet1["J3"]
-old_application_number = application.value
-print(application.value)
-application.value = application.value + 1
-new_application_number = application.value
+# Increment application number
+if noe_token == 0:
+    old_application_number = application.value
+    print(application.value)
+    application.value = application.value + 1
+    new_application_number = application.value
+else:
+    owner = sheet1["C4"]
+    owner.value = bill_to
+    project = sheet1["E4"]
+    project.value = ship_to
 
 # increment work period
 
@@ -83,47 +127,50 @@ print(workperiod)
 # change invoice number to the new invoice number
 # J9
 invoice_number = sheet1["J9"]
-old_invoice_number = invoice_number.value
-invoice_number.value = new_inv
+
 # add work from this period to completed and stored to date
 completed_through_cell = sheet1["E26"]
-completed_through_cell.value += completed_through
-
-# add old current payment E39 to previous payments E38
-retention_percentage = sheet1['B29'].value
-sheet1["E38"].value += sheet1["E39"].value
-sheet1["E39"].value = completed_through * (1-retention_percentage*.01)
-
-
-# Page 2
-sheet2 = workbook["G703"]
-prev_apps = ["D13", "D17", "D21", "D25"]
-this_period = ["E13", "E17", "E21", "E25"]
-
-for x, y in zip(prev_apps, this_period):
-    sheet2[x].value += sheet2[y].value
-    sheet2[y].value = 0
+if noe_token == 0:
+    completed_through_cell.value += completed_through
+    old_invoice_number = invoice_number.value
+    invoice_number.value = new_inv
+    # add old current payment E39 to previous payments E38
+    retention_percentage = sheet1['B29'].value
+    sheet1["E38"].value += sheet1["E39"].value
+    sheet1["E39"].value = completed_through * (1-retention_percentage*.01)
+if noe_token == 1:
+    completed_through_cell.value = completed_through
+    invoice_number.value = new_inv
+    current_payment_due = sheet1["E39"]
+    current_payment_due = (completed_through * .9)
 
 
-# Add old this period to previous applications
-#D13 E13
-# Each new row increases by 4 (D17 E17)
+
+if noe_token == 0:
+    # Page 2
+    sheet2 = workbook["G703"]
+    prev_apps = ["D13", "D17", "D21", "D25"]
+    this_period = ["E13", "E17", "E21", "E25"]
+
+    for x, y in zip(prev_apps, this_period):
+        sheet2[x].value += sheet2[y].value
+        sheet2[y].value = 0
+
+
+
 
 # Save a new file with new name
-new_workbook_path = workbook_path.replace(str(old_invoice_number), str(new_inv))
-new_workbook_path = new_workbook_path.split(' ')
-new_workbook_path[-1] = new_workbook_path[-1].replace(str(old_application_number), str(new_application_number))
-new_workbook_path = (' ').join(new_workbook_path)
-print(new_workbook_path)
-#new_workbook_path = new_workbook_path.replace(str(old_application_number), str(new_application_number))
+if noe_token == 0:
+    new_workbook_path = workbook_path.replace(str(old_invoice_number), str(new_inv))
+    new_workbook_path = new_workbook_path.split(' ')
+    new_workbook_path[-1] = new_workbook_path[-1].replace(str(old_application_number), str(new_application_number))
+    new_workbook_path = (' ').join(new_workbook_path)
+    print(new_workbook_path)
 
-
-#print(new_workbook_path)
-#print(type(new_workbook_path))
-
-#print(new_workbook_path)
-#workbook.save(new_workbook_path)
-workbook.save(new_workbook_path)
+    workbook.save(new_workbook_path)
+else:
+    workbook.save(str(owner.value + project.value + str(new_inv) + str(application.value)))
+## Here I am
 
 # TODO: Add formulae to all cells
 
---
+++ new_contract_invoice.py
@@ -0,0 +1,141 @@
+# Auto GUI
+# This is a program that grabs information from a specified 
+# Excel column and then automates the input of that list into a data entry form
+# using autogui
+
+
+
+## TODO:
+# I think I might be able to use tkinter to confirm when I've made a copy that way I can just automate
+# the whole first part of the making a copy step and then once I've done pressed the button 
+# that one time it'll do the rest for me
+import pyautogui, openpyxl, time, pyperclip, datetime, calendar
+import os, re, sys
+import pygetwindow as gw
+from openpyxl import load_workbook
+from pyautogui import write, press, keyUp, keyDown, hotkey
+from time import sleep
+from datetime import datetime, timedelta, date
+
+from tkinter import Tk, simpledialog, messagebox
+from tkinter.filedialog import askopenfilename
+
+def copy_clipboard():
+    hotkey('ctrl', 'c')
+    time.sleep(.5)
+    return pyperclip.paste()
+
+def highlight_line():
+    press('numlock')
+    keyDown('shiftleft')
+    press('end')
+    keyUp('shiftleft')
+    sleep(1)
+    press('numlock')
+    sleep(1)
+
+def highlight_3_line():
+    hotkey('ctrl', 'home')
+    press('home')
+    press('numlock')
+    keyDown('shiftleft')
+    press('end')
+    press('down', presses=2)
+    keyUp('shiftleft')
+    sleep(1)
+    press('numlock')
+    sleep(1)
+
+windows = gw.getAllWindows()
+
+qb_window = None
+
+for window in windows:
+    if "QuickBooks" in window.title:
+        qb_window = window
+        break
+
+# Create the Tkinter root window
+root = Tk()
+root.withdraw()  # Hide the root window
+
+contract_amount = simpledialog.askinteger("Contract Prompt", "What is the total contract amount:")
+
+completed_through= simpledialog.askinteger("Invoice Prompt", "Enter the amount billed without retention taken out:")
+
+qb_window.activate()
+hotkey('ctrl', 't')
+time.sleep(.5)
+press('up', presses=25)
+press('down', presses=3)
+hotkey('alt', 't')
+sleep(2)
+
+bill_bin = messagebox.askyesno("Confirmation", "Have you entered the billing information?")
+sleep(.5)
+qb_window.activate()
+hotkey('alt', 'j')
+time.sleep(.5)
+
+sleep(1)
+
+today = date.today()
+print(today)
+res = calendar.monthrange(today.year, today.month)[1]
+completed_date = f"Completed through {today.month}/{res}/{today.year}"
+
+
+# starts once you have created a copy of the invoice, which will
+# start with highlighting the customer job
+# 8 tabs to the first item
+# 10 tabs to the first price cell
+
+# get the new invoice number
+press('tab', presses=3)
+new_inv = copy_clipboard()
+
+press('tab')
+highlight_3_line()
+bill_to = copy_clipboard()
+sleep(1)
+
+press('tab', presses=2)
+highlight_3_line()
+ship_to = copy_clipboard()
+sleep(.5)
+
+
+press('tab', presses=4)
+write(str(contract_amount))
+press('down', presses=1)
+sleep(.5)
+write(str(completed_through))
+sleep(.5)
+hotkey('shift', 'tab')
+
+
+highlight_line()
+
+press('backspace')
+write(completed_date)
+press('tab')
+
+press('down')
+write('-10%')
+
+
+print_bin = messagebox.askyesno("Confirmation", "Do you want to print this?")
+
+if print_bin:
+    sleep(1)
+    qb_window.activate()
+    hotkey('ctrl', 'p')
+    sleep(5)
+    press('space') 
+else:
+    sys.exit()
+
+
+# This is some code from ChatGPT that lets you find a file by the number
+# even if it's not the only thing in the file name
+
