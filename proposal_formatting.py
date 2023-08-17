4354e0c43ce6c4cdee802d4cc2d89b095e59c6e1
Worm <mwormley008@gmail.com>
2023-08-17 12:25:13 -0500

    proposal formatter functional

--- proposal_formatting.py
+++ proposal_formatting.py
@@ -1,6 +1,6 @@
-import pyautogui
+import pyautogui, pyperclip
 import time
-from pyautogui import press, write, hotkey
+from pyautogui import press, write, hotkey, keyDown, keyUp
 from time import sleep
 
 # Define a helper function to send keys with a delay
@@ -11,6 +11,39 @@ def repeat_hotkey(times, delay, *keys):
     for _ in range(times):
         pyautogui.hotkey(*keys)
         sleep(delay)
+
+def copy_clipboard():
+    hotkey('ctrl', 'c')
+    time.sleep(.5)
+    return pyperclip.paste()
+
+def highlight_line():
+    press('numlock')
+    keyDown('shiftleft')
+    press('end')
+    keyUp('shiftleft')
+    sleep(1)
+    press('numlock')
+    sleep(1)
+
+def highlight_home_line():
+    press('numlock')
+    keyDown('shiftleft')
+    press('home')
+    keyUp('shiftleft')
+    sleep(1)
+    press('numlock')
+    sleep(1)
+
+def search_term(term):
+    pyautogui.hotkey('ctrl', 'f')
+    sleep(1)
+    pyautogui.write(term)
+    sleep(1)
+    pyautogui.press('return')
+    sleep(1)
+    pyautogui.press('esc')
+    sleep(1)
 
 ## Add a focus function for .doc files
 
@@ -97,45 +130,39 @@ press('space')
 print("Script completed.")
 
 def NameSave():
-    # Define a helper function to send keys with a delay
-    def send_keys_with_delay(keys, delay=0.01):
-        pyautogui.write(keys, interval=delay)
-
     # Search for "proposal submitted to:"
-    pyautogui.hotkey('ctrl', 'f')
-    send_keys_with_delay('proposal submitted to:')
-    pyautogui.press('return')
-    pyautogui.press('esc')
-    send_keys_with_delay('downendshifthome', delay=0.15)
-    pyautogui.hotkey('ctrl', 'c')
-    send_keys_with_delay('upupctrlv ')
+    search_term('proposal submitted to:')
+    send_keys_with_delay('down', 'end')
+    highlight_line()
+    contractor = copy_clipboard()
+    
 
     # Search for "Work to be performed at:"
-    pyautogui.hotkey('ctrl', 'f')
-    send_keys_with_delay('Work to be performed at:')
-    pyautogui.press('return')
-    pyautogui.press('esc')
-    send_keys_with_delay('downendshifthome', delay=0.14)
-    pyautogui.hotkey('ctrl', 'c')
-    send_keys_with_delay('upupendctrlv ')
+    search_term('Work to be performed at:')
+    send_keys_with_delay('down', 'end', delay=0.14)
+    highlight_line()
+    job_name = copy_clipboard()
+    sleep(1)
+    
 
     # Search for "Work to be performed at:" again
-    pyautogui.hotkey('ctrl', 'f')
-    send_keys_with_delay('Work to be performed at:')
-    time.sleep(1)
-    pyautogui.press('return')
-    pyautogui.press('esc')
-    time.sleep(2)
-    send_keys_with_delay('downdowndownend', delay=0.14)
-    send_keys_with_delay('ctrlleftleftleftshifthome', delay=0.1)
-    pyautogui.hotkey('ctrl', 'c')
-    send_keys_with_delay('upupupupendctrlvshifthome', delay=0.13)
-    pyautogui.hotkey('ctrl', 'x')
-    pyautogui.press('return')
+    
+    send_keys_with_delay('down', 'down', 'end', delay=0.14)
+    
+    # alright here I think I'd like to take the whole line instead af then take the part before the comma
+
+    highlight_home_line()
+        
+    job_loc = copy_clipboard()
+
+    job_loc = job_loc.split(',')[0]
+    print(job_loc)
+
+    
     time.sleep(1)
     pyautogui.press('f12')
     time.sleep(2)
-    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.write(contractor+ ' ' + job_name + ' ' + job_loc)
     time.sleep(1)
 
 NameSave()
