1ac744ba7b68a8d9b498c18d5f02ba83e1df553b
Worm <mwormley008@gmail.com>
2023-10-04 13:01:52 -0500

    powershell script

---
+++ folderSearch.ps1
@@ -0,0 +1,42 @@
+﻿$fileName = Read-Host -Prompt 'Enter a File Name'
+
+# Set the path to the folder you want to search
+$folderPath = '\\WBR\shared\Proposals'
+
+# Search for .doc files that match the user's input in the specified folder
+if ($docFile = Get-ChildItem -Path $folderPath -Filter "*$fileName*.doc" -File)
+{
+    & {
+        for ($i = 0; $i -lt $docFile.Count; $i++)
+        {
+            [pscustomobject]@{
+                Index = $i + 1
+                DocName = $docFile[$i].BaseName
+                Created = $docFile[$i].CreationTime
+            }
+        }
+    } | Format-Table -AutoSize | Out-String -Stream
+
+    # Let the user choose which file(s) to open based on the Index
+    $selection = Read-Host -Prompt 'Enter DOC number(s) to open'
+
+    # If the user's input contains a comma, split by comma and trim spaces
+    # Otherwise, treat it as a single integer
+    if ($selection -contains ',') {
+        $selectedFiles = $selection -split ',' | ForEach-Object { $_.Trim() }
+    } else {
+        $selectedFiles = @($selection)
+    }
+
+    foreach ($selected in $selectedFiles)
+    {
+        $doc = $docFile[([int]$selected) - 1]
+        # Open the selected file with Microsoft Word
+        Start-Process -FilePath "WINWORD.EXE" -ArgumentList "`"$($doc.FullName)`""
+        Write-Host $doc
+    }
+}
+else
+{
+    Write-Host -Object "No file with name `"$fileName`" found."
+}
--- sumcombinations.py
+++ sumcombinations.py
@@ -1,6 +1,6 @@
 import itertools
 
-numbers = [1180, 1120, 3739, 3330, 2698]
+numbers = [750, 1200, 750, 6000, 3040, 600, 900, -5745]
 
 combinations = []
 for r in range(1, len(numbers) + 1):
@@ -8,7 +8,8 @@ for r in range(1, len(numbers) + 1):
 
 for comb in combinations:
     comb_sum = sum(comb)
-    if comb_sum == 8660:
+    print(comb_sum)
+    if comb_sum == 3064:
         print(f"Combination: {comb}  Sum: {comb_sum}")
 else:
     print("No other matching combination(s) found.")
