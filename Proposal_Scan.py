b078cda7b8044c3e73ce0f5700f72c1502695b78
Worm <mwormley008@gmail.com>
2024-02-09 11:31:14 -0600

    added email

--- Proposal_Scan.py
+++ Proposal_Scan.py
@@ -127,6 +127,7 @@ if __name__ == '__main__':
             'Tessler':'kurt@tesslerinc.com',
             'test':'wbrroof@gmail.com; throwod@gmail.com',
             'Tim':'briana@timcoteinc.com',
+            'J.':'j jdivita.associates@yahoo.com',
             }
 
         proposal_message = 'Hello,\nPlease see attached proposal.\n\nThank you,\nMichael Wormley\nWBR Roofing\n25084 W Old Rand Rd\nWauconda, IL 60084\n​O: 847-487-8787​\nwbrroof@aol.com'
